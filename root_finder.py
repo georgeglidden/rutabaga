fixed numpy overflow errors with a pure pythonic implementation of generate_Qs

root_finder.py
@@ -37,32 +37,33 @@ class RootFinder():
         polly = np.trim_zeros(polly)
         # print(polly)
         if len(polly)<2: # no roots!
-            return [], None
+            return []
         try:
-            roots, error = mpmath.polyroots(polly, error=True, maxsteps=self.maxsteps)
+            roots = mpmath.polyroots(polly, extraprec=1000, maxsteps=self.maxsteps)
         except:
             self.didnotconverge += 1
-            return [], None
-        return roots, error
+            return []
+        return roots
 
     def generate_to(self, output_file):
         """
         Goes through the inputted polynomials, feeds them to generate_roots, and saves in increments to avoid overloading memory.
         :return:
         """
-        roots_list = pd.DataFrame(columns=['real', 'imaginary', 'q', 'error'])
+        roots_list = pd.DataFrame(columns=['real', 'imaginary', 'p' ,'q'])
         for i in trange(len(self.pols.columns)-1):
             key = self.pols.columns[i+1] # ignore the "unnamed = 0" string.
+            p = key.strip('][').split(',')[0]
             q = key.strip('][').split(',')[1]
             polly = self.pols[key].to_numpy()
-            roots, error = self.generate_roots(polly)
+            roots = self.generate_roots(polly)
             # print(roots)
             for root in roots:
                 # print(roots_list)
                 roots_list = roots_list.append({'real':str(root.real),
                                    'imaginary':str(root.imag),
+                                   'p':str(p),
                                    'q':str(q),
-                                   'error':str(error)
                                    }, ignore_index=True)
             if i % self.batch_size == 0:  # every batch_size iterations, save progress.
                 # print("save triggered!")
@@ -72,13 +73,13 @@ class RootFinder():
                     # print("before deleting",roots_list)
                     roots_list.to_csv(output_file, mode='a', header=False)
                     del roots_list
-                    roots_list = pd.DataFrame(columns=['real', 'imaginary', 'q', 'error'])
+                    roots_list = pd.DataFrame(columns=['real', 'imaginary', 'p', 'q'])
         roots_list.to_csv(output_file, mode='a', header=False)
         print(roots_list)
         print(f"{self.didnotconverge} didn't converge with max steps {self.maxsteps}")
 
 
 if __name__=='__main__':
-    max_denom = 130
-    rf = RootFinder(input_file=f'data/q_to_denom_{max_denom}.csv', precision=10, maxsteps=100)
+    max_denom = 100
+    rf = RootFinder(input_file=f'data/q_to_denom_{max_denom}.csv', precision=10, maxsteps=200)
     rf.generate_to(f'data/roots_{max_denom}.csv')